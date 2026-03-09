3f47f7a541558675998916e41abd94d1618a70b6
Dhanush216 <dhanushchandra1296@gmail.com>
2026-03-09 16:11:13 +0530

    Terraform fmt check corrected

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -2,7 +2,7 @@ resource "helm_release" "prometheus_stack" {
   name = "kube-prometheus-stack"
 
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart = "kube-prometheus-stack"
+  chart      = "kube-prometheus-stack"
 
   namespace = "monitoring"
 
