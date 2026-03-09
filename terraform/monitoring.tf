a778ff6f96d6d882ef6e4e32bf9b23996daa03a8
Dhanush216 <dhanushchandra1296@gmail.com>
2026-03-09 16:09:29 +0530

    Configuring Prometheus and Grafana Dashborad

---
+++ terraform/monitoring.tf
@@ -0,0 +1,14 @@
+resource "helm_release" "prometheus_stack" {
+  name = "kube-prometheus-stack"
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart = "kube-prometheus-stack"
+
+  namespace = "monitoring"
+
+  create_namespace = true
+
+  values = [
+    file("${path.module}/prometheus-values.yaml")
+  ]
+}
