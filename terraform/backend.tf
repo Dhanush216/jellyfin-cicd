441afcf36ea704dba31d16065c7e2985f81b8ddc
Dhanush216 <dhanushchandra1296@gmail.com>
2026-03-11 12:09:26 +0530

    Updating backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,13 +4,17 @@ terraform {
     key    = "jellyfin/terraform.tfstate"
     region = "us-east-1"
 
-    endpoint = "http://127.0.0.1:9000"
+    endpoints = {
+      s3 = "http://127.0.0.1:9000"
+    }
 
     access_key = "minioadmin"
     secret_key = "minioadmin"
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    force_path_style            = true
+    skip_requesting_account_id  = true
+
+    force_path_style = true
   }
 }
