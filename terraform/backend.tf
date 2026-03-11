dc7d4448375123e42079c186539e5ae731f03154
Dhanush216 <dhanushchandra1296@gmail.com>
2026-03-11 12:01:59 +0530

    Adding Terraform remote backend locally using minio

---
+++ terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "jellyfin/terraform.tfstate"
+    region = "us-east-1"
+
+    endpoint = "http://127.0.0.1:9000"
+
+    access_key = "minioadmin"
+    secret_key = "minioadmin"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
